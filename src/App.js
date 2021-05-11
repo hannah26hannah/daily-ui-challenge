7df0096761ef79efb55a31d765cc1dd8197ea234
hannah26hannah <ooyoonoo24@gmail.com>
2021-05-11 23:43:38 +0900

    ✨ setup react-router links

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 import Main from "./views/Main";
+import Code from './views/Code';
 
 function App() {
-  return <Main />;
+  return (
+    <Router basename='/daily-ui-challenge'>
+      <Switch>
+        <Route path='/code'>
+          <Code />
+        </Route>
+        <Route exact path='/'>
+          <Main />
+        </Route>
+      </Switch>
+    </Router>
+  )
 }
 
 export default App;
