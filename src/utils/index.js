0d7030256bcb147de7b496bddf1a852523347ac4
hannah26hannah <ooyoonoo24@gmail.com>
2021-05-11 23:45:05 +0900

    ✨ add util functions

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,17 @@
+import { useLocation } from 'react-router-dom';
+
 export function spliceDateString(stringDate) {
   return stringDate.substring(0, 10);
 }
+
+// 'Sign Up/In' -> 'sign-up-in'
+export function getProperPath(dataTitle) {
+  return dataTitle.toLowerCase().split(/\s|\//).join('-')
+}
+
+export function useQuery() {
+  return new URLSearchParams(useLocation().search)
+}
+
+
+
